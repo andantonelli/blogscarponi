dfbc3834bb3e5ae96c54e9a76cd0875734f082e1
Andrea Antonelli <antonelli@incode.it>
2018-09-26 00:18:45 +0200

    match template and style

--- wp-content/themes/blogscarponi/template-parts/content.php
+++ wp-content/themes/blogscarponi/template-parts/content.php
@@ -23,33 +23,33 @@
 	</div>
 	
 	<?php if($match_review): ?>
-		<div class="post__match-review match">
+		<div class="post__match-review py3 match">
 			<?php while(has_sub_field('match_review')): 
 					$match = get_sub_field('match');
 					$review = get_sub_field('review'); ?>
 
-				<div class="match__header flex items-center">
+				<div class="match__header pb2 flex items-center">
 					<?php while(have_rows('match')): the_row();
 						$home_team = get_sub_field('home_team');
 						$home_score = get_sub_field('home_score');
 						$away_team = get_sub_field('away_team');
 						$away_score = get_sub_field('away_score'); ?>
 
-						<div class="match__home flex items-center">
+						<div class="match__result match__home <?php echo $home_score > $away_score ? 'match__winner' : '' ?> flex items-center">
 							<?php $post = $home_team; setup_postdata($post); ?>
-								<h2 class="match__team m0"><?php echo get_the_title(); ?></h2>
-								<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+								<h2 class="match__team right-align regular uppercase m0 px2"><?php echo get_the_title(); ?></h2>
+								<!-- <div class="match__logo" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>') no-repeat center center; background-size: contain; width: 5rem; height: 5rem;"></div> -->
 							<?php wp_reset_postdata(); ?>
-							<span class="match__score"><?php echo $home_score; ?></span>
+							<span class="match__score center inline-block px1 pt1 bold"><?php echo $home_score; ?></span>
 						</div>
 						
-						<span class="match__separetor inline-block px2"><?php echo 'v' ?></span>
+						<span class="match__separetor px1 inline-block"><?php echo 'v' ?></span>
 						
-						<div class="match__away flex items-center">
-							<span class="match__score"><?php echo $away_score; ?></span>							
+						<div class="match__result match__away <?php echo $away_score > $home_score ? 'match__winner' : '' ?> flex items-center">
+							<span class="match__score center inline-block px1 pt1 bold"><?php echo $away_score; ?></span>							
 							<?php $post = $away_team; setup_postdata($post); ?>
-								<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-								<h2 class="match__team m0"><?php echo the_title(); ?></h2>
+								<!-- <div class="match__logo" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>') no-repeat center center; background-size: contain; width: 5rem; height: 5rem;"></div> -->
+ 								<h2 class="match__team regular uppercase m0 px2"><?php echo the_title(); ?></h2>
 							<?php wp_reset_postdata(); ?>						
 						</div>
 
@@ -62,4 +62,6 @@
 			<?php endwhile; ?>
 		</div>
 	<?php endif; ?>
+
+	<nav><a class="post__back-link button" href="<?php echo get_post_type_archive_link('post'); ?>">Tutti gli articoli</a></nav>
 </article>

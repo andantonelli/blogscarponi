b20b4c349d74e7116268958a80e21b5fac64bf25
Andrea Antonelli <antonelli@incode.it>
2018-10-01 19:03:37 +0200

    match section revamp

--- wp-content/themes/blogscarponi/template-parts/content.php
+++ wp-content/themes/blogscarponi/template-parts/content.php
@@ -23,42 +23,44 @@
 	</div>
 	
 	<?php if($match_review): ?>
-		<div class="post__match-review my3 match">
+		<div class="post__match-review my3">
 			<?php while(has_sub_field('match_review')): 
 					$match = get_sub_field('match');
 					$review = get_sub_field('review'); ?>
 
-				<div class="match__header pb2 flex items-center">
-					<?php while(have_rows('match')): the_row();
-						$home_team = get_sub_field('home_team');
-						$home_score = get_sub_field('home_score');
-						$away_team = get_sub_field('away_team');
-						$away_score = get_sub_field('away_score'); ?>
+				<div class="match mb4">
+					<div class="match__header pb2 flex items-center">
+						<?php while(have_rows('match')): the_row();
+							$home_team = get_sub_field('home_team');
+							$home_score = get_sub_field('home_score');
+							$away_team = get_sub_field('away_team');
+							$away_score = get_sub_field('away_score'); ?>
 
-						<div class="match__result match__home <?php echo $home_score > $away_score ? 'match__winner' : '' ?> flex items-center">
-							<?php $post = $home_team; setup_postdata($post); ?>
-								<h2 class="match__team right-align regular uppercase m0 px2"><?php echo get_the_title(); ?></h2>
-								<!-- <div class="match__logo" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>') no-repeat center center; background-size: contain; width: 5rem; height: 5rem;"></div> -->
-							<?php wp_reset_postdata(); ?>
-							<span class="match__score center inline-block px1 pt1 bold"><?php echo $home_score; ?></span>
-						</div>
-						
-						<span class="match__separetor px1 inline-block"><?php echo 'v' ?></span>
-						
-						<div class="match__result match__away <?php echo $away_score > $home_score ? 'match__winner' : '' ?> flex items-center">
-							<span class="match__score center inline-block px1 pt1 bold"><?php echo $away_score; ?></span>							
-							<?php $post = $away_team; setup_postdata($post); ?>
-								<!-- <div class="match__logo" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>') no-repeat center center; background-size: contain; width: 5rem; height: 5rem;"></div> -->
- 								<h2 class="match__team regular uppercase m0 px2"><?php echo the_title(); ?></h2>
-							<?php wp_reset_postdata(); ?>						
-						</div>
+							<div class="match__result match__home <?php echo $home_score > $away_score ? 'match__winner' : '' ?> flex items-center">
+								<?php $post = $home_team; setup_postdata($post); ?>
+									<h2 class="match__team right-align regular uppercase m0 px2"><?php echo get_the_title(); ?></h2>
+									<!-- <div class="match__logo" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>') no-repeat center center; background-size: contain; width: 5rem; height: 5rem;"></div> -->
+								<?php wp_reset_postdata(); ?>
+								<span class="match__score center inline-block px1 pt1 bold"><?php echo $home_score; ?></span>
+							</div>
+							
+							<span class="match__separetor px1 inline-block hide sm-hide"><?php echo 'v' ?></span>
+							
+							<div class="match__result match__away <?php echo $away_score > $home_score ? 'match__winner' : '' ?> flex items-center">
+								<span class="match__score center inline-block px1 pt1 bold"><?php echo $away_score; ?></span>							
+								<?php $post = $away_team; setup_postdata($post); ?>
+									<!-- <div class="match__logo" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>') no-repeat center center; background-size: contain; width: 5rem; height: 5rem;"></div> -->
+									<h2 class="match__team regular uppercase m0 px2"><?php echo the_title(); ?></h2>
+								<?php wp_reset_postdata(); ?>						
+							</div>
 
-					<?php endwhile; ?>
-				</div>
+						<?php endwhile; ?>
+					</div>
 
-				<div class="match__content wysiwyg">
-					<?php echo $review; ?>
-				</div>			
+					<div class="match__content wysiwyg">
+						<?php echo $review; ?>
+					</div>	
+				</div>		
 			<?php endwhile; ?>
 		</div>
 	<?php endif; ?>
